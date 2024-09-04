d097d010b203510dbfe2724e99546435943f48e8
Maksym Vasylyshyn <maksym.vasylyshyn@dataart.com>
2024-09-04 18:50:12 +0300

    update service urls

--- src/WebUI/src/app/modules/menu/services/capital.service.ts
+++ src/WebUI/src/app/modules/menu/services/capital.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Capital } from '../models/capital-model';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../../../../environments/environment';
+import { environment } from '../../../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
--- src/WebUI/src/app/shared/services/exchange.service.ts
+++ src/WebUI/src/app/shared/services/exchange.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Exchange } from '../../core/models/exchange-model';
-import { environment } from '../../../environments/environment';
+import { environment } from '../../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
