b0b110f7938ca26de19dcf8d67b2dd4dcb8bcfdb
Maksym Vasylyshyn <maksym.vasylyshyn@dataart.com>
2025-03-29 19:40:47 +0200

    added

--- src/FinanceTracker/FinanceTracker.Api/Program.cs
+++ src/FinanceTracker/FinanceTracker.Api/Program.cs
@@ -12,6 +12,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Host.UseSerilogDependencies();
 
+builder.Configuration.AddEnvironmentVariables();
+
 builder.Services
     .AddApplication()
     .AddApi()
