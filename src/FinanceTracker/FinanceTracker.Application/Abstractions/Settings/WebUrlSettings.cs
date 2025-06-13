fac85e4f3f3df3b1068313032f70b0a7529321b7
Maksym Vasylyshyn <maxim24152771@gmail.com>
2025-06-13 22:06:26 +0300

    fixed WebUISettings (#27)

--- src/FinanceTracker/FinanceTracker.Application/Abstractions/Settings/WebUrlSettings.cs
+++ src/FinanceTracker/FinanceTracker.Application/Abstractions/Settings/WebUrlSettings.cs
@@ -2,7 +2,5 @@ namespace FinanceTracker.Application.Abstractions.Settings;
 
 public sealed class WebUrlSettings
 {
-    public required string BaseAddress { get; init; }
-
-    public required string LocalAddress { get; init; }
+    public required string UIUrl { get; init; }
 }
